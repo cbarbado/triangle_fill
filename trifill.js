30153364dbf141bea10cd1d3b5251370d8873702
Cesar <barbado@barbado.com.br>
2023-07-14 09:38:27 +0200

    code improvements

--- trifill.js
+++ trifill.js
@@ -1,13 +1,13 @@
-var canvas;
-var context;
+let canvas;
+let context;
 const canvasWidth  = 800;
 const canvasHeight = 600;
-var vertex_X     = new Array();
-var vertex_Y     = new Array();
+let vertex_X     = new Array();
+let vertex_Y     = new Array();
 
 function prepareCanvas()
 {
-	var canvasDiv = document.getElementById('canvasDiv');
+	let canvasDiv = document.getElementById('canvasDiv');
 	canvas = document.createElement('canvas');
 	canvas.setAttribute('width', canvasWidth);
 	canvas.setAttribute('height', canvasHeight);
@@ -20,7 +20,7 @@ function prepareCanvas()
 	context = document.getElementById('canvas').getContext("2d");
 
 	context.fillStyle             = "#000000";
-    context.lineWidth             = 1;
+   context.lineWidth             = 1;
 
 	$('#canvas').mousedown(function(e)
 	{
@@ -37,14 +37,14 @@ function prepareCanvas()
 function addVertex(x, y)
 {
 	/* TODO: check if the vertex is not already in the array */
-    vertex_X.push(x);
-    vertex_Y.push(y);
+   vertex_X.push(x);
+   vertex_Y.push(y);
 }
 
 function clearCanvas()
 {
 	vertex_X.length = 0;
-    vertex_Y.length = 0;
+   vertex_Y.length = 0;
 	context.clearRect(0, 0, canvasWidth, canvasHeight);
 }
 
@@ -54,15 +54,28 @@ function setPixel (x,y) {
     context.fillRect(x, y,1,1);
 }
 
+async function half_fill(v1x, v1y, v2x, v2y, d1x, d1y, d2x, d2y) {
+	let stepY = (v1y > v2y) ? -1 : 1;
+	for (let y = v1y; y != (v2y + stepY); y += stepY) {
+		let p1x = Math.round(v1x + (d1x * ((y-v1y)/d1y)));
+		let p2x = Math.round(v2x + (d2x * ((y-v2y)/d2y)));
+
+		let stepX = (p1x > p2x) ? -1 : 1;
+		for(let x = p1x; x != (p2x + stepX); x += stepX) {
+			setPixel(x,y);
+		}
+	}
+}
+
 async function trifill()
 {
-	var top = 0;
-	var bot = 0;
-	var mid = 0;
+	let top = 0;
+	let bot = 0;
+	let mid = 0;
 
 	/* TODO: check if it is a triangle and not a line. */
 
-	for(var i = 1; i < vertex_X.length; i++) {
+	for(let i = 1; i < vertex_X.length; i++) {
 		if(vertex_Y[i] < vertex_Y[top]) {
 			top = i;
 		}
@@ -75,59 +88,28 @@ async function trifill()
 		mid ++;
 	}
 
-  	/* TOP -> MID */
-  	var delta_x1 = vertex_X[top] - vertex_X[bot];
-  	var delta_y1 = vertex_Y[top] - vertex_Y[bot];
-  	var delta_x2 = vertex_X[top] - vertex_X[mid];
-  	var delta_y2 = vertex_Y[top] - vertex_Y[mid];
-  	var delta_x3 = vertex_X[mid] - vertex_X[bot];
-  	var delta_y3 = vertex_Y[mid] - vertex_Y[bot];
+  	let delta_x1 = vertex_X[top] - vertex_X[bot];
+  	let delta_y1 = vertex_Y[top] - vertex_Y[bot];
+  	let delta_x2 = vertex_X[top] - vertex_X[mid];
+  	let delta_y2 = vertex_Y[top] - vertex_Y[mid];
+  	let delta_x3 = vertex_X[mid] - vertex_X[bot];
+  	let delta_y3 = vertex_Y[mid] - vertex_Y[bot];
 
-    for (var y = vertex_Y[top]; y <= vertex_Y[mid]; y++) {
-    	var pos_x1 = vertex_X[top] + (delta_x1 * ((y-vertex_Y[top])/delta_y1));
-    	var pos_x2 = vertex_X[mid] + (delta_x2 * ((y-vertex_Y[mid])/delta_y2));
-
-	    if(pos_x2 > pos_x1) {
-	    	for(var x = pos_x1; x <= pos_x2; x++) {
-			    setPixel(x,y);
-	    	}
-	    }
-	    else {
-	    	for(var x = pos_x2; x <= pos_x1; x++) {
-			    setPixel(x,y);
-	    	}
-	    }
-    }
-
-    /* MID -> BOT */
-    for (var y = vertex_Y[mid]; y <= vertex_Y[bot]; y++) {
-    	var pos_x1 = vertex_X[top] + (delta_x1 * ((y-vertex_Y[top])/delta_y1));
-    	var pos_x2 = vertex_X[mid] + (delta_x3 * ((y-vertex_Y[mid])/delta_y3));
-
-	    if(pos_x2 > pos_x1) {
-	    	for(var x = pos_x1; x <= pos_x2; x++) {
-			    setPixel(x,y);
-	    	}
-	    }
-	    else {
-	    	for(var x = pos_x2; x <= pos_x1; x++) {
-			    setPixel(x,y);
-	    	}
-	    }
-    }
+	half_fill(vertex_X[top], vertex_Y[top], vertex_X[mid], vertex_Y[mid], delta_x1, delta_y1, delta_x2, delta_y2);
+	half_fill(vertex_X[bot], vertex_Y[bot], vertex_X[mid], vertex_Y[mid], delta_x1, delta_y1, delta_x3, delta_y3);
 }
 
 function redraw()
 {
-  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+   context.clearRect(0, 0, context.canvas.width, context.canvas.height);
   
-  if(3 == vertex_X.length)
-  {
-  	trifill();
-  }
-  else {
-	for(var i=0; i < vertex_X.length; i++) {		
-		context.fillRect(vertex_X[i], vertex_Y[i],3,3);
-	}
-  }
+   if(3 == vertex_X.length)
+   {
+  	  trifill();
+   }
+   else {
+	   for(let i=0; i < vertex_X.length; i++) {		
+		   context.fillRect(vertex_X[i], vertex_Y[i],3,3);
+	   }
+   }
 }
